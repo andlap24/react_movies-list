5411e022020505f59e10f09558d10540ec954d0f
andlap24 <andrew.laptev24@gmail.com>
2020-10-21 14:19:11 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8b91e5646cd49f0c22bf066e7ff906ac.js"
+  "./precache-manifest.037933d8634456f8cb5b74f87c46c47d.js"
 );
 
 self.addEventListener('message', (event) => {
